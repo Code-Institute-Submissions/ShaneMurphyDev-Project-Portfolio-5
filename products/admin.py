d0b9462594ee67d848e59479dad9c6edd090ae8d
Shane Murphy <MurphyShaneDev@gmail.com>
2023-12-19 13:06:03 +0000

    updated admin models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from .models import Product, Painting, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Product)
 admin.site.register(Painting)
 admin.site.register(Category)
