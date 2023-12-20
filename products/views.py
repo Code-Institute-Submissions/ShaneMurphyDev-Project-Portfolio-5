8ae2158846b143ff7eef283db2e20d68e7b5b416
Shane Murphy <MurphyShaneDev@gmail.com>
2023-12-20 10:25:01 +0000

    added product detail view

--- products/views.py
+++ products/views.py
@@ -14,3 +14,19 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+from django.shortcuts import render
+
+from .models import Product
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
