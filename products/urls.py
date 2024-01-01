c66cec995a16e5a3470eda0bc04124ee2feafe62
Shane Murphy <MurphyShaneDev@gmail.com>
2024-01-01 19:02:35 +0000

    updated urls

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
